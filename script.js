11cedaf25a16e2f505ba000183d9961b7de54206
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-24 10:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let userDetails = document.querrySelector('form');
+
+userDetails.addEventListener('onsubmit', promiseResolve);
+
+function promiseResolve(event){
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(document.getElementById('age').value >= 18){
+				resolve(alert(`Welcome, ${document.getElementById('name').value}. You can vote.`));
+			}
+			resolve(alert(`Welcome, ${document.getElementById('name').value}. You aren't old enough.`));
+		}, 4 * 1000);
+	});
+}
